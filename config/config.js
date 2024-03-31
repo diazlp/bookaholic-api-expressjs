e7971d3ef26fea6799931efe4a04abfda1a8c1e1
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-03-31 11:22:58 +0700

    chore: attempt vercel failed deployment

--- config/config.js
+++ config/config.js
@@ -1,11 +1,16 @@
 const dbConfig = {
   "development": {
-    "username": process.env.DB_USERNAME || "postgres",
-    "password": process.env.DB_PASSWORD || "postgres",
-    "database": process.env.DB_NAME,
-    "host": process.env.HOST || "127.0.0.1",
-    "dialect": "postgres",
-    "logging": false
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME,
+    host: process.env.HOST || "127.0.0.1",
+    dialect: "postgres",
+    dialectModule: require('pg'),
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    },
+    logging: false,
   }
 }
 
