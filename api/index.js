435317ead9420d0ea28f62b9c9e6274eb818d512
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-03-31 12:18:54 +0700

    fix: fix swagger appears broken on vercel deployed

--- api/index.js
+++ api/index.js
@@ -19,6 +19,8 @@ app.use('/categories', categoryRoutes)
 app.use('/books', bookRoutes)
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
   customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.css'
 }));
 
